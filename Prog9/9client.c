7e3e3cb4752d86740f58d0648fef020dc2e07bc5
Kiran Shenoy P.H <33525945+KiranShny@users.noreply.github.com>
2017-11-23 13:25:21 +0530

    Create 9client.c

---
+++ Prog9/9client.c
@@ -0,0 +1,23 @@
+//client
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+int main()
+{
+int soc,n;
+char buffer[1024],fname[50];
+struct sockaddr_in addr;
+soc=socket(PF_INET,SOCK_STREAM,0);
+addr.sin_family=AF_INET;
+addr.sin_port=htons(8099);
+addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+while(connect(soc,(struct sockaddr *)&addr,sizeof(addr)));
+printf("\nClient connected to server");
+printf("\nEnter file name ");
+scanf("%s",fname);
+send(soc,fname,sizeof(fname),0);
+printf("\nRecived response\n");
+while((n=recv(soc,buffer,sizeof(buffer),0))>0)
+printf("%s",buffer);
+return 0;
+}
